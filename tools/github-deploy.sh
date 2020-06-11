05e94241236a55ea638c859f9c8e3319d3cf2c89
Ken Wills <ken@zaptillion.net>
2020-06-11 16:15:08 -0500

    test

--- tools/github-deploy.sh
+++ tools/github-deploy.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 # this is the path to the repo the build should run from. It should not begin with a leading /.
-WILDFLY_REPO="${1:-wildfly/wildfly-s2i}"
+WILDFLY_REPO="${1:-wildfly}"
 WILDFLY_REF="${2:-master}"
 WILDFLY_IMAGE_TAG="${3:-latest}"
 
-if [ "${1}" == "--help" ]; then
- cat << EOF
+function help {
+cat << EOF
    
-   ${0} <github repo> <git ref> <tag to apply to image>
-   ${0} wildfly/wildfly-s2i master latest
+Usage: ${0} <github repo> <git ref> <tag to apply to image>
+
+       ${0} wildfly master latest
+        - this will deploy from the wildfly/wildfly-s2i repo, using the master 
+          branch and tag the image with :latest
+
+Arguments:
+        repo:    the git repo to run the deployment in, for example the value 
+                 luck3y would indicate the luck3y/wildfly-s2i repo
+        git ref: the branch / tag from which the deployment should be created
+        tag:     the docker tag to apply to the built image
 
 EOF
- exit
-fi
+}
 
 if [ -z "${WILDFLYS2I_GITHUB_PAT}" ];
 then
@@ -22,12 +30,23 @@ then
     echo "The token should have the following permissions: repo:status, repo_deployment, public_repo, read:packages, read:discussion, workflow"
     echo "Then export the token in your current shell: export WILDFLYS2I_GITHUB_PAT=... and re-run this script. Your deployment action will run as the production deploy action in github actions."
     echo
-else
-  curl \
-    -X POST \
-    -H "Authorization: token $WILDFLYS2I_GITHUB_PAT" \
-    -H "Accept: application/vnd.github.ant-man-preview+json" \
-    -H "Content-Type: application/json" \
-    https://api.github.com/repos/${WILDFLY_REPO}/deployments \
-    --data "{\"ref\": \"${WILDFLY_REF}\", \"required_contexts\": [], \"environment\": \"production\", \"payload\": {\"image_tag\": \"${WILDFLY_IMAGE_TAG}\"}}"
+    exit
 fi
+
+if [ "${1}" == "--help" ]; then
+ help
+ exit
+fi
+
+if [ -n "${1}" ] && [ $# -lt 3 ]; then
+  help
+  exit
+fi
+
+echo curl \
+  -X POST \
+  -H "Authorization: token $WILDFLYS2I_GITHUB_PAT" \
+  -H "Accept: application/vnd.github.ant-man-preview+json" \
+  -H "Content-Type: application/json" \
+  https://api.github.com/repos/${WILDFLY_REPO}/wildfly-s2i/deployments \
+  --data "{\"ref\": \"${WILDFLY_REF}\", \"required_contexts\": [], \"environment\": \"production\", \"payload\": {\"image_tag\": \"${WILDFLY_IMAGE_TAG}\"}}"
